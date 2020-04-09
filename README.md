b25204854fb6496b4bbce8d8a3fb14e1a18eb9ac
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-09 15:33:38 -0400

    add demo gif

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # test-material-ui
-> Testing Material UI components using Cypress
+> Testing [Material UI](https://material-ui.com/) components using Cypress
+
+Uses [cypress-react-unit-test v2](https://github.com/bahmutov/cypress-react-unit-test/pull/108)
 
 Installation
 
 ```shell
+$ npx create-react-app test-material-ui
+$ cd test-material-ui
+
+$ yarn add @material-ui/core @material-ui/icons @material-ui/lab
+info Direct dependencies
+├─ @material-ui/core@4.9.9
+├─ @material-ui/icons@4.9.1
+└─ @material-ui/lab@4.0.0-alpha.48
+
 $ yarn add -D cypress-react-unit-test@3.0.0-cypress-mount-mode.14
 ```
 
 Add [cypress.json](cypress.json) with experimental flag
 
-Write components and specs in [src](src) folder.
+Write components and specs in [src](src) folder. I recommend looking at [src/autocomplete-spec.js](src/autocomplete-spec.js)
+
+![Autocomplete test](images/autocomplete-demo.gif)
--- src/autocomplete-spec.js
+++ src/autocomplete-spec.js
@@ -9,20 +9,6 @@ import TextField from '@material-ui/core/TextField'
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import { top100Films } from './top-100-movies'
 
-export default function ComboBox() {
-  return (
-    <Autocomplete
-      id="combo-box-demo"
-      options={top100Films}
-      getOptionLabel={option => option.title}
-      style={{ width: 300 }}
-      renderInput={params => (
-        <TextField {...params} label="Combo box" variant="outlined" fullWidth />
-      )}
-    />
-  )
-}
-
 it('finds my favorite movie', () => {
   cy.viewport(500, 700)
   mount(
@@ -50,4 +36,13 @@ it('finds my favorite movie', () => {
     .and('have.class', 'MuiAutocomplete-option')
     .click()
   cy.get('#combo-box-demo').should('have.value', 'The Godfather')
+
+  // now delete a couple of letter
+  cy.focused().type('{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}')
+  // should show two search results
+  cy.get('.MuiAutocomplete-option').should('have.length', 2)
+    .and(el$ => {
+      expect(el$[0]).to.have.text('The Godfather')
+      expect(el$[1]).to.have.text('The Godfather: Part II')
+    })
 })
